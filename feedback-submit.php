e50ce86f80c8e494447fddefdeb3f1aced1d46ec
RONIT SINGH <ronitsingh7003@gmail.com>
2022-01-29 19:10:35 +0530

    Feedback added

---
+++ feedback-submit.php
@@ -0,0 +1,26 @@
+<?php
+include 'connection.php';
+$id=['id'];
+$food=$_POST['fq_name'];
+$msg=$_POST['fq_msg'];
+$q="INSERT INTO feedback(id,food,message) VALUES('$id','$food','$msg');";
+$r=mysqli_query($conn,$q);
+if($r==1)
+{
+    
+        echo ' <script type="text/javascript">
+        alert(" Sent Successfully!!");
+        location="feedback.php";
+        </script>';
+    
+}
+else
+{
+    
+        echo ' <script type="text/javascript">
+        alert(" UnSuccessful :(");
+        location="feedback.php";
+        </script>';
+    
+}
+?>
