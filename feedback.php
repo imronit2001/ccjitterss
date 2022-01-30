41cf424b9b8a2a6127c120bfd67d207b07d465bc
DEBOJYOTI2019 <debojyotidas9294@gmail.com>
2022-01-30 12:13:25 +0530

    sales page responsive changes

--- feedback.php
+++ feedback.php
@@ -11,7 +11,7 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-    <link rel="stylesheet" href="css/feedback.css">
+    <link rel="stylesheet" href="feedback.css">
     <title>Feedback</title>
   </head>
   <body>
--- sales-admin.php
+++ sales-admin.php
@@ -15,6 +15,7 @@ include 'connection.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/sales-style.css">
+    <link rel="stylesheet"  media="screen and (max-width:902px)" href="css/mobile.css">
     <title>Employee</title>
   </head>
   <body>
